8ca97713ecd84a537dc8e96aa9354fad7822803e
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-03-08 19:21:37 +0100

    Switch to `bare-bundle-evaluate`

--- boot.js
+++ boot.js
@@ -1,11 +1,13 @@
 const fs = require('fs')
-const runBundle = require('node-bare-bundle')
+const { pathToFileURL } = require('url')
+const Bundle = require('bare-bundle')
+const evaluate = require('bare-bundle-evaluate')
 
 const isMac = process.platform === 'darwin'
 const dir = (isMac ? '../../../../../../../' : '../../../../../../')
 
-require.extensions['.bundle'] = function (mod, filename) {
-  mod.exports = runBundle(fs.readFileSync(filename), { mount: filename })
+require.extensions['.bundle'] = function (module, filename) {
+  module.exports = evaluate(Bundle.from(fs.readFileSync(filename)).mount(pathToFileURL(filename + '/')))
 }
 
 try {
