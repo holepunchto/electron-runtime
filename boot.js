a97e7e148321476992d62c06c02b0a9eabcdab53
dmc <david.mark.clements@gmail.com>
2025-12-08 20:26:15 +0100

    bootstrap Pear global with runtime information

--- boot.js
+++ boot.js
@@ -4,18 +4,21 @@ const Bundle = require('bare-bundle')
 const evaluate = require('bare-bundle-evaluate')
 
 const isMac = process.platform === 'darwin'
+const isWindows = process.platform === 'win32'
 const dir = (isMac ? '../../../../../../../' : '../../../../../../')
 
 require.extensions['.bundle'] = function (module, filename) {
   module.exports = evaluate(Bundle.from(fs.readFileSync(filename)).mount(pathToFileURL(filename + '/')))
 }
 
-try {
-  require(dir + 'boot.bundle')
-} catch {
-  try {
-    require(dir + 'boot.cjs')
-  } catch {
-    require(dir + 'boot.js')
-  }
+const rtiFlagIx = process.argv.indexOf('--rti')
+const RTI = rtiFlagIx > -1 && process.argv[rtiFlagIx + 1]
+const state = RTI ? null : JSON.parse(process.argv.slice(isWindows ? -2 : -1)[0])
+
+class API {
+  static RTI = RTI ? JSON.parse(RTI) : state.rti // runtime information
 }
+
+global.Pear = new API()
+
+require(dir + API.RTI.APPID + '.bundle')
