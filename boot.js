7e768b3e5b3289e125c91f45e7deb10622073b07
dmc <david.mark.clements@gmail.com>
2025-12-09 20:43:13 +0100

    boot argv[1]

--- boot.js
+++ boot.js
@@ -3,19 +3,8 @@ const { pathToFileURL } = require('url')
 const Bundle = require('bare-bundle')
 const evaluate = require('bare-bundle-evaluate')
 
-const isMac = process.platform === 'darwin'
-const dir = (isMac ? '../../../../../../../' : '../../../../../../')
-
 require.extensions['.bundle'] = function (module, filename) {
   module.exports = evaluate(Bundle.from(fs.readFileSync(filename)).mount(pathToFileURL(filename + '/')))
 }
 
-try {
-  require(dir + 'boot.bundle')
-} catch {
-  try {
-    require(dir + 'boot.cjs')
-  } catch {
-    require(dir + 'boot.js')
-  }
-}
+require(process.argv[1])
