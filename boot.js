730627c9389c0743a0957a61a05cafbada62ae83
dmc <david.mark.clements@gmail.com>
2025-12-09 22:09:17 +0100

    respect file: urls

--- boot.js
+++ boot.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
-const { pathToFileURL } = require('url')
+const { pathToFileURL, fileURLToPath } = require('url')
 const Bundle = require('bare-bundle')
 const evaluate = require('bare-bundle-evaluate')
-
+const Module = require('module')
+const resolveFilename = Module._resolveFilename
+Module._resolveFilename = function (request, parent, isMain, options) {
+  if (typeof request === 'string' && request.startsWith('file:')) request = fileURLToPath(request)
+  return resolveFilename.call(this, request, parent, isMain, options)
+}
 require.extensions['.bundle'] = function (module, filename) {
   module.exports = evaluate(Bundle.from(fs.readFileSync(filename)).mount(pathToFileURL(filename + '/')))
 }
