79b854fb15cc9f8419fca50a50baef4a61e3075a
dmc <david.mark.clements@gmail.com>
2025-12-10 01:17:29 +0100

    better to just pass in path instead of support file url

--- boot.js
+++ boot.js
@@ -1,13 +1,7 @@
 const fs = require('fs')
-const { pathToFileURL, fileURLToPath } = require('url')
+const { pathToFileURL } = require('url')
 const Bundle = require('bare-bundle')
 const evaluate = require('bare-bundle-evaluate')
-const Module = require('module')
-const resolveFilename = Module._resolveFilename
-Module._resolveFilename = function (request, parent, isMain, options) {
-  if (typeof request === 'string' && request.startsWith('file:')) request = fileURLToPath(request)
-  return resolveFilename.call(this, request, parent, isMain, options)
-}
 require.extensions['.bundle'] = function (module, filename) {
   module.exports = evaluate(Bundle.from(fs.readFileSync(filename)).mount(pathToFileURL(filename + '/')))
 }
