1dc293578871afa02087398f09f076873eccbdaa
dmc <david.mark.clements@gmail.com>
2025-12-11 14:56:10 +0100

    path.resolve entry

--- boot.js
+++ boot.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 const { pathToFileURL } = require('url')
 const Bundle = require('bare-bundle')
 const evaluate = require('bare-bundle-evaluate')
@@ -6,5 +7,5 @@ require.extensions['.bundle'] = function (module, filename) {
   module.exports = evaluate(Bundle.from(fs.readFileSync(filename)).mount(pathToFileURL(filename + '/')))
 }
 const argv = process.type === 'renderer' ? process.argv.filter(([ch]) => ch !== '-') : process.argv
-const entry = argv[1]
+const entry = path.resolve(argv[1])
 require(entry)
